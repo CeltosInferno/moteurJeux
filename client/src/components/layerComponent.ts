72b609323fe2fb91ae4e1a6adcacb56310acbbaf
LadyCookie <bouzerea@etu.insa-toulouse.fr>
2019-10-06 16:59:28 -0400

    appel à display dans layercomponent OK

--- client/src/components/layerComponent.ts
+++ client/src/components/layerComponent.ts
@@ -29,56 +29,43 @@ export class LayerComponent extends Component<object> implements IDisplayCompone
     if (layerSprites.length === 0) {
       return;
     }
-    else{
-      console.log("Il y a : " +  layerSprites.length);
-      //On récupère la feuille de sprites
-      const spriteSheet = layerSprites[0].spriteSheet;
 
+    //On récupère la feuille de sprites
+    const spriteSheet = layerSprites[0].spriteSheet;
 
-      this.vertices = new Float32Array(4 * TextureComponent.vertexSize * layerSprites.length);
-      this.indices = new Uint16Array(6 * layerSprites.length);
-      var indice =0;
-      for(const sprite of layerSprites){
-          //trouver points
-        this.vertices[indice*4]=sprite.vertices[0];
-        this.vertices[indice*4+1]=sprite.vertices[1];
-        this.vertices[indice*4+2]=sprite.vertices[2];
-        this.vertices[indice*4+3]=sprite.vertices[3];
-        //const indices = new Uint16Array([0, 1, 2, 2, 3, 0]);
-        this.indices[indice*6] = indice*4;
-        this.indices[indice*6+1] = indice*4 + 1;
-        this.indices[indice*6+2] = indice*4 + 2;
-        this.indices[indice*6+3] = indice*4 + 2;
-        this.indices[indice*6+4] = indice*4 + 3;
-        this.indices[indice*6+5] = indice*4;
+    
+    let v: any[] = [];
+    let i: any[] = [];
+    var indice =0;
 
-        
-        indice +=1;
-      }
-      console.log(this.indices);
-      
-      //On créer le buffer et on bind les buffer
-      this.vertexBuffer = GL.createBuffer()!;
-      GL.bindBuffer(GL.ARRAY_BUFFER, this.vertexBuffer);
-      GL.bufferData(GL.ARRAY_BUFFER, this.vertices, GL.DYNAMIC_DRAW);
-      this.indexBuffer = GL.createBuffer()!;
-      GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-      GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, this.indices, GL.DYNAMIC_DRAW);
-
-      /////////////
-      //DISPLAY
-      ////////////
-      GL.bindBuffer(GL.ARRAY_BUFFER, this.vertexBuffer);
-      GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-      spriteSheet.bind();
-      GL.drawElements(GL.TRIANGLES, this.indices.length, GL.UNSIGNED_SHORT, 0);
-      //spriteSheet.unbind();
+    //On ajoute tous les sprites à rendre
+    for(const sprite of layerSprites){
+      const spriteIndex = indice * 4;
+          v = v.concat(Array.prototype.slice.call(sprite.vertices));
+          i = i.concat([spriteIndex, spriteIndex + 1, spriteIndex + 2, spriteIndex + 2, spriteIndex + 3, spriteIndex]);
+          indice++;
     }
-    
-    
 
+    this.indices = new Uint16Array(i);
+    this.vertices = new Float32Array(v);
 
-    
+    //On crée le buffer et on bind les buffer
+    //Buffer de vertex
+    this.vertexBuffer = GL.createBuffer()!;
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.vertexBuffer);
+    GL.bufferData(GL.ARRAY_BUFFER, this.vertices, GL.DYNAMIC_DRAW);
+    //Buffer d'index
+    this.indexBuffer = GL.createBuffer()!;
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, this.indices, GL.DYNAMIC_DRAW);
+
+    /////////////
+    //DISPLAY
+    ////////////
+    spriteSheet.bind();
+    GL.drawElements(GL.TRIANGLES, this.indices.length, GL.UNSIGNED_SHORT, 0);
+    spriteSheet.unbind();
+
   }
 
   // ## Fonction *listSprites*
